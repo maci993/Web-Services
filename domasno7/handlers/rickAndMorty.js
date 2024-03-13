7915f4327b82927e64f3e51c5aa8fb8830d4f04e
Marina Stojanovic <145804225+maci993@users.noreply.github.com>
2024-03-13 20:16:51 +0100

    Update rickAndMorty.js

--- domasno7/handlers/rickAndMorty.js
+++ domasno7/handlers/rickAndMorty.js
@@ -2,7 +2,8 @@ const { getCharacter } = require("../pkg/rickAndMorty");
 
 const getCharacterRickAndMorty = async (req, res) => {
   try {
-    const data = await getCharacter(req.params.id);
+    const new_id = Number(req.params.id)
+    const data = await getCharacter(new_id);
     return res.status(200).send(data);
   } catch (err) {
     return res.status(500).send("Internal Server Error");
